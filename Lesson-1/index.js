dd0087cfb4334977ed03a38aa164832f9b6df282
= <ismoilbek20050421@gmail.com>
2023-02-13 22:26:35 +0500

    JavaScript Course: Lesson-1

---
+++ Lesson-1/index.js
@@ -0,0 +1,34 @@
+// console.log("Lesson-1");
+// console.error("Error");
+// console.warn("Warning");
+
+// const person = {
+//   name: "John Doe",
+//   age: 23
+// };
+
+// console.table(person);
+
+// console.clear();
+
+// console.log("first code");
+
+
+// console.clear();
+
+// console.log(12 - 28);
+
+// console.og("Hello World!");
+
+
+// console.log(1);
+
+// setTimeout(function() {
+//   console.log(2);
+// },2000);
+
+// console.log(3);
+
+// let a = 1;
+// let b = 1;
+// console.log({} === {});
