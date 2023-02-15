bc4ef7f11b06339a97dd5dd5371347c2a477cb0e
= <ismoilbek20050421@gmail.com>
2023-02-15 23:00:09 +0500

    JavaScript Course: Lesson-2

---
+++ Lesson-2/README.md
@@ -0,0 +1,13 @@
+# JavaScript Fundamentals
+
+https://javascript.info/first-steps
+
+https://javascript.info/hello-world
+
+https://javascript.info/structure
+
+https://javascript.info/variables
+
+https://javascript.info/types
+
+https://javascript.info/alert-prompt-confirm
---
+++ Lesson-2/index2.js
@@ -0,0 +1,78 @@
+"use strict";
+
+
+// const person1 = {
+//   userName: "Anna",
+//   age: 23
+// }
+
+// const person2 = {
+//   userName: "Anna",
+//   age: 23
+// }
+
+// console.log(person1 === person2);
+
+// Comment
+
+// Ctrl + /
+// Ctrl + Shift + / || alt shift A
+
+//
+/*  */
+
+// let text = 1; [].forEach();
+
+// let a = (b = c = d = 10);
+
+// console.log(a, b, c, d);
+
+// let text = 1;
+// text = 2;
+
+// console.log(text);
+
+// var text = "1";
+// var text = "2";
+// console.log(text);
+
+// const variable = 10;
+// variable = 11;
+// console.log(variable);
+
+// alert("Enter"); // ogohlantirish
+
+// console.log("Console Enter")
+
+// const isConfirm = confirm("How old are you ? 18 yoshdan kattamisan ?");
+// if(isConfirm === true){
+//   console.log("Ha men 18 yoshdan kattaman");
+// }else {
+//   console.log("Ha men 18 yoshdan kichikman");
+// }
+
+// const text = prompt("What is your name ?","Ismoil");
+// console.log(text);
+
+// String
+
+// "", '', ``
+
+// let firstName = "John";
+// let age = 17;
+// console.log(typeof `My name is ${firstName}. I'm ${age} years old.`);
+
+// Number 
+// let age = -185;
+// console.log(typeof age);
+
+// console.log(typeof -1 / 0);
+
+// let age = null;
+// let a;
+// console.log(typeof age);
+// console.log(typeof a);
+
+// let sym = Symbol("id");
+
+// console.log(typeof sym)
