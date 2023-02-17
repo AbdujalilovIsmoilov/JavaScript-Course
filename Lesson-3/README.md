74e4e20888a49a2836691e976312e1b7ec068e6c
= <ismoilbek20050421@gmail.com>
2023-02-18 00:38:21 +0500

    JavaScript Course: Lesson-3

---
+++ Lesson-3/README.md
@@ -0,0 +1,11 @@
+# JavaScript Fundamentals
+
+https://javascript.info/type-conversions
+
+https://javascript.info/operators
+
+https://javascript.info/comparison
+
+https://javascript.info/ifelse
+
+https://javascript.info/logical-operators
---
+++ Lesson-3/index.js
@@ -0,0 +1,111 @@
+"use strict";
+
+// const userName = "John";
+// const lastName = "Doe";
+// console.log(`My name is ${userName}.
+// My surename is  ${lastName}
+// I am ${10 + 20} years old.`);
+
+// console.log("Hello \nWorld!");
+
+// const userName = "John";
+// const lastName = "Doe";
+// console.log("My name is John. \nMy surename is Doe. \nI am 30 years old.");
+// let index = 100;
+// index + "%";
+
+// Type Conversion
+// String
+
+// const index = 125 + "";
+// const index = String(undefined);
+
+// Number();
+// const index = Number("125");
+// const index = parseInt(22.9);
+// console.log(index);
+
+// NaN => Not a Number
+
+// const index = "John" * 2;
+// console.log(typeof(index));
+// console.log(index);
+
+// const text = "John Doe";
+// console.log(Boolean(text));
+
+// Falthy values: "", null, undefined, false, 0, NaN
+
+// let index = 1;
+// // index += 1; // 1 + 1
+// index = 1 + index;
+// index = index + 1;
+// console.log(index);
+// console.log(index);
+
+// let index = 5;
+// // index++
+// --index;
+// console.log(index);
+// let index = 0;
+// function increment() {
+//   index++;
+//   count.textContent = index;
+// }
+
+// function decrement() {
+//   index--;
+//   if (index < 0) {
+//     index = 0;
+//     alert("Not a Found");
+//   } else {
+//     count.textContent = index;
+//   }
+// }
+
+// Comparison Operators
+
+// <, >, <=, >=, =, ==, ===
+
+// const a = "11";
+// const b = 11;
+// console.log(a == b);
+
+// console.log(null === undefined);
+
+// const index = Number(prompt("Hafta kunini yozing!"));
+// if(index == 1){
+//   console.log("Dushanba");
+// }else if(index == 2){
+//   console.log("Seshanba");
+// }else if(index == 3){
+//   console.log("Chorshanba");
+// }else if(index == 4){
+//   console.log("Payshanba");
+// }else if(index == 5){
+//   console.log("Juma");
+// }else if(index == 6){
+//   console.log("Shanba");
+// }else if(index == 7){
+//   console.log("Yakshanba");
+// }else {
+//   console.log("Bunday hafta kuni mavjud emas!");
+// }
+
+// Logical Operators
+// ||, && , !
+
+// console.log("1" || true);
+// console.log(true && true);
+// console.log(true && false);
+
+// const face = true;
+// const id = false;
+
+// if (id || face) {
+//   console.log("Open");
+// } else {
+//   console.log("Close");
+// }
+
+// console.log(!!!!!!!!!!!!!!!1);
